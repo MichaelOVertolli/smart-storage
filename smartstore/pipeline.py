ccf47aea992fa79deff5728a55c0466c754caa0f
Mike Cichonski <mike.cichonski@gmail.com>
2018-10-14 15:49:37 -0400

    changes to import and transformation functions

--- smartstore/pipeline.py
+++ smartstore/pipeline.py
@@ -28,13 +28,12 @@ def get_json():
     return glob('./smartstore/json/*')
 
 
-def processJSON (json_files, currentpath, local, plot):
+def processJSON (json_files, currentpath, plot):
     '''process each json file
 
-    data:   the data contained in the json file
+    data:           the data contained in the json file
     currentpath:    the root path of the database
-    local:  True if database is located locally, False if on web
-    plot:   number of different views plotted per frame
+    plot:           number of different views plotted per frame
     '''
 
     for jn, jfile in enumerate(json_files):
@@ -43,7 +42,7 @@ def processJSON (json_files, currentpath, local, plot):
 
 
         # parse json data into variables
-        name = data['name']
+        loc = data['name']
         date = data['date']
         frames = data['frames']
         objects = data['objects']
@@ -62,12 +61,12 @@ def processJSON (json_files, currentpath, local, plot):
 
         # get camera intrinsics
         K = np.transpose(np.reshape(readvaluesfromtxt(os.path.join \
-                         (currentpath,'data',name,'intrinsics.txt'),local),(3,3)))
+                         (currentpath,'data',name,'intrinsics.txt')),(3,3)))
 
         # get camera extrinsics
         ex_file = os.path.listdir(os.path.join('data',name,'extrinsics'))[-1] 
         extrinsicsC2W = np.transpose(np.reshape(readvaluesfromtext(os.path.join \
-          (currentpath,'data',name,'extrinsics',ex_file),local), \
+          (currentpath,'data',name,'extrinsics',ex_file)), \
           (-1,3,4)),(1,2,0))
 
         for i, f in enumerate(frames):
@@ -97,7 +96,7 @@ def processJSON (json_files, currentpath, local, plot):
             current_frame = Frame(i,width,height)
             current_frame.loc = name
             current_frame.background = background
-            current_frame.depthMap = depthread(depth,local)
+            current_frame.depthMap = depthread(depth)
             current_frame.intrinsics = K
             current_frame.extrinsics = getextrinsics(extrinsicsC2W,i)
             exceptions   = []
@@ -150,60 +149,85 @@ def processJSON (json_files, currentpath, local, plot):
 ### IMPORT TOOLS
 ###############################################################################
 
-def readvaluesfromtxt(filename,local):
-    fid = open(filename,'r')
-    values = [map(lambda y: float(y), x.split(' ')) for x in
-              ''.join(fid.read().decode('utf-8')).split('\n') if x!='']
-    fid.close()
-    return values
+def get_intrinsics(loc):
+    with open(os.path.join(os.getcwd(),'data',loc,'intrinsics.txt'),'r') as fid:
+        values = [map(lambda y: float(y), x.split(' ')) for x in
+                  ''.join(fid.read().decode('utf-8')).split('\n') if x!='']
+    intrinsics = np.transpose(np.reshape(values),(3,3))
+    return intrinsics 
 
-def getextrinsics(extrinsicsC2W,frame):
-    extrinsics = [[extrinsicsC2W[0][0][frame],extrinsicsC2W[0][1][frame], \
-                   extrinsicsC2W[0][2][frame],extrinsicsC2W[0][3][frame]],\
-                  [extrinsicsC2W[1][0][frame],extrinsicsC2W[1][1][frame], \
-                   extrinsicsC2W[1][2][frame],extrinsicsC2W[1][3][frame]],\
-                  [extrinsicsC2W[2][0][frame],extrinsicsC2W[2][1][frame], \
-                   extrinsicsC2W[2][2][frame],extrinsicsC2W[2][3][frame]]]
+def get_extrinsics(loc,frame_id):
+    with open(os.path.listdir(os.path.join('data',loc,'extrinsics'))[-1],'r') as fid:
+        values = [map(lambda y: float(y), x.split(' ')) for x in
+                  ''.join(fid.read().decode('utf-8')).split('\n') if x!='']
+    # get extrinsics for all frames:
+    ext = np.transpose(np.reshape(values,(-1,3,4)),(1,2,0))
+    # get extrinsics for current frame_id:
+    extrinsics = [[ext[0][0][frame_id],ext[0][1][frame_id], \
+                   ext[0][2][frame_id],ext[0][3][frame_id]],\
+                  [ext[1][0][frame_id],ext[1][1][frame_id], \
+                   ext[1][2][frame_id],ext[1][3][frame_id]],\
+                  [ext[2][0][frame_id],ext[2][1][frame_id], \
+                   ext[2][2][frame_id],ext[2][3][frame_id]]]
     return extrinsics
 
-def depthread(filename,local):
-    depth = Image.open(filename,'r')
-    depthmap = np.array(depth)
-    depth.close()
-    bitshift = [[(d >> 3) or (d << 16-3) for d in row] for row in depthmap]
-    depthmap = [[float(d)/1000.0 for d in row] for row in bitshift]
-    return depthmap
+# frame_id must be an int
+def get_depth(loc,frame_id):
+    depth_dir  = os.path.join(os.getcwd(),'data',loc,'depth')
+    depth_list = os.path.listdir(depth_path)
+    for img in depth_list:
+        file_num = "0"*(7-len(str(1+frame_id*5)))+str(1+frame_id*5)+"-"
+        if file_num in img:
+            depth_path = os.path.join(depth_dir,img)
+            break
+    with Image.open(depth_path,'r') as depth:
+        depth_map = np.array(depth)
+    bit_shift = [[(d >> 3) or (d << 16-3) for d in row] for row in depth_map]
+    depth_map = [[float(d)/1000.0 for d in row] for row in bit_shift]
+    return depth_map
 
-def depth2xyzcamera(K,depth):
+def depth2camera(depth_map,K):
     [x,y] = np.meshgrid(range(1,641),range(1,481))
     xyzcamera = []
     l = np.multiply([[d - K[0][2] for d in row] for row in x], \
-                    [[d / K[0][0] for d in row] for row in depth])
+                    [[d / K[0][0] for d in row] for row in depth_map])
     xyzcamera.append(l)
     l = np.multiply([[d - K[1][2] for d in row] for row in y], \
-                    [[d / K[1][1] for d in row] for row in depth])
+                    [[d / K[1][1] for d in row] for row in depth_map])
     xyzcamera.append(l)
-    xyzcamera.append(depth)
-    depth = [[int(bool(n)) for n in row] for row in depth]
-    xyzcamera.append(depth)
+    xyzcamera.append(depth_map)
+    depth_map = [[int(bool(n)) for n in row] for row in depth_map]
+    xyzcamera.append(depth_map)
     return xyzcamera
 
-def camera2XYZworld(xyzcamera,extrinsics):
+def camera2world(xyz_camera,extrinsics):
     xyz = []
-    for j in range(0,len(xyzcamera[0][0])):
-        for i in range(0,len(xyzcamera[0])):
-            if xyzcamera[3][i][j]:
-                data = (xyzcamera[0][i][j],\
-                        xyzcamera[1][i][j],\
-                        xyzcamera[2][i][j])
+    for j in range(0,len(xyz_camera[0][0])):
+        for i in range(0,len(xyz_camera[0])):
+            if xyz_camera[3][i][j]:
+                data = (xyz_camera[0][i][j],\
+                        xyz_camera[1][i][j],\
+                        xyz_camera[2][i][j])
                 xyz.append(data)
-    xyzworld = transformpointcloud(xyz, extrinsics)
-    vals = (xyzworld, xyzcamera[3])
-    return vals
 
-def transformpointcloud(xyz, rt):
-    rt_ = [(float(t[0]),float(t[1]),float(t[2])) for t in rt]
-    return np.matmul(rt_, np.transpose(xyz))
+    rt = [(float(t[0]),float(t[1]),float(t[2])) for t in extrinsics]
+    xyz_world = np.matmul(rt,np.transpose(xyz))
+    # xyz_camera[3] are the valid world coords corresponding to each [x,y]
+    xyz_world = (xyz_world, xyz_camera[3])
+    return xyz_world
+
+# xyd is a (type='frame', loc, frame_id) tuple
+def depth2world(xyd):
+    assert(xyd[0]=='frame')
+    intrinsics = get_intrinsics(xyd[1])        # get intrinsics for current location
+    extrinsics = get_extrinsics(xyd[1],xyd[2]) # get extrinsics for current frame
+    depth_map  = get_depth(xyd[1],xyd[2])      # get depth for current frame
+    
+    xyz_camera = depth2camera(depth_map,intrinsics)  # calculate camera coords
+    xyz_world  = camera2world(xyz_camera,extrinsics) # calculate world coords
+
+    # return a (type='world_matrix',loc,frame_id, xyz_world) tuple
+    return ('world_matrix',xyd[1],xyd[2],xyz_world)
 
 ###############################################################################
 ### MAIN RUN PROCEDURE
@@ -211,5 +235,14 @@ def transformpointcloud(xyz, rt):
 
 if __name__ == "__main__":
 
-    objects = {} # { ID : [name,colour,frame_inds,oldIDs] }
-    frames  = {} # 
+    # something like this:
+    
+    # iterate over all JSON files
+
+    frames = # get frame IDs for current json file
+
+    for i,frame in enumerate(frames):
+        # insert stuff from processJSON() above that reads loc from json file
+        xyd = ('frame',loc,i) # create tuple to pass into depth2world below
+        xyz_world = depth2world(xyd)
+        processJSON() # TODO: a new version of this function that only processes the labels
